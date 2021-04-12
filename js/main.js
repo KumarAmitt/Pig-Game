2fc5ca40603786f1c9d5ced2462160425bdd8f01
KumarAmitt <amitt2015@outlook.com>
2021-04-12 18:13:25 +0530

    Add init function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 let scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 
+
+//ROLL DICE BUTTON
 document.querySelector('.btn-roll').addEventListener('click', function(){
   //1. Random Number
   let dice = Math.floor(Math.random() * 6 + 1);
@@ -28,18 +21,68 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   }else{
     //Next Player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0'
-    document.getElementById('current-1').textContent = '0'
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer()
   }
 });
+
+//HOLD BUTTON
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+  //Ad CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  //Update the UI
+
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //Check if player won the game
+  if (scores[activePlayer] >= 20){
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  }else {
+    nextPlayer();
+  }
+
+});
+
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
 
 
 

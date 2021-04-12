27bdd6351991fe4118a8a88327f02a1cab7ce61b
KumarAmitt <amitt2015@outlook.com>
2021-04-12 17:30:28 +0530

    Add toggle functionality for active player and the next player

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -22,7 +22,23 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   diceDOM.src = 'img/dice-' + dice + '.png';
 
   //3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1){
+    //Add score
+    roundScore += dice
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else{
+    //Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
 
 
